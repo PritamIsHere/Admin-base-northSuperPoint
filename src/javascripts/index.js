689fefe3189febcef40dea9d3702bfdbe3f36bef
PritamIsHere <pritamkar98765@gmail.com>
2024-04-02 02:30:28 +0530

    register added

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,12 +1,27 @@
 $(document).ready(function () {
+    // menu toggle
     $('#toggleBtn').click(function () {
         $('#toggleItems').toggle(500);
     });
 
+
+    // modal start
     $('#tip_modal_open').click(function () {
         $('#tip_modal').show(200);
     });
     $('#tip_modal_close').click(function () {
         $('#tip_modal').hide(200);
     });
+    // modal end
+
+    // page switch
+    $("#dashboard-triger").click(function (e) {
+        $("#register").hide(200);
+        $("#dashboard").show(200);
+    });
+    $("#register-triger").click(function (e) {
+        $("#dashboard").hide(200);
+        $("#register").show(200);
+    });
+
 });
